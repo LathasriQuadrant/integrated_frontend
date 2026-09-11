e626fdce37e555e12e9c8764655e09201cde327c
LathasriQuadrant <lathasri.uddemari@quadranttechnologies.com>
2026-09-11 15:17:30 +0530

    Update PreMigrationAnalysis.tsx

--- src/pages/PreMigrationAnalysis.tsx
+++ src/pages/PreMigrationAnalysis.tsx
@@ -14,7 +14,7 @@ import {
   TrendingUp,
   TrendingDown,
 } from "lucide-react";
-import AppLayout from "@/components/layout/AppLayout";
+import AppLayout from "@/components/layout/AppLayout"; 
 import { Button } from "@/components/ui/button";
 import {
   Table,
